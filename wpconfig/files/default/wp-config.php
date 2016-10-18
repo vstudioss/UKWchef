22be317f93c3929bb14c24cf9991f535b99981ef
vstudioss <chriswheels@visionaries.studio>
2016-10-18 12:39:06 +0100

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -64,7 +64,7 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'DreamsComeTrue_';
 
 /**
  * For developers: WordPress debugging mode.
