6684c2da7ac349b5fbe760b678b525e8023d2fa7
vstudioss <chriswheels@visionaries.studio>
2016-10-20 20:31:00 +0100

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', '');
  
-define('WP_HOME','http://passion-760600027.us-west-1.elb.amazonaws.com');
-define('WP_SITEURL','http://passion-760600027.us-west-1.elb.amazonaws.com');
+define('WP_HOME','http://wordpress-990739050.us-east-1.elb.amazonaws.com');
+define('WP_SITEURL','http://wordpress-990739050.us-east-1.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
